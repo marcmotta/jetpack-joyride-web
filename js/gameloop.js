2dc95b31bec07644332145e5ceeaf12ae1e62d33
Marcelo Agostinho <magostinhojr@gmail.com>
2021-04-30 04:35:46 -0300

    Nao fazer jogo em HTML

--- js/gameloop.js
+++ js/gameloop.js
@@ -1,116 +1,112 @@
 function start(){
      
-     var pontos = 0;
+    var score = 0;
 
-     $("#inicio").hide(); 
-     $("#gameplay").append("<div class='pontos' id='pontos'>" + pontos + "</div>");
-     $("#gameplay").append("<div class='missile'></div>");
-     $("#gameplay").append("<div class='player'></div>");
-     $("#gameplay").append("<div class='obstacle'></div>");
-     $("#gameplay").append("<div class='scientist'></div>");
+     //Object Area
+     $("#start").hide(); 
+     $("#gameplay").append("<div class='score' id='score'>" + score + "</div>");
+     $("#gameplay").append("<div class='missile' id='missile'></div>");
+     $("#gameplay").append("<div class='player' id='player'></div>");
+     $("#gameplay").append("<div class='obstacle' id='obstacle'></div>");
+     $("#gameplay").append("<div class='scientist' id='scientist'></div>");
+     $("#gameplay").append("<div class='coin' id='coin'></div>");
 
-     var trilha = document.getElementById("trilha");
-     var audioExplosao = document.getElementById("audioExplosao");
-     function executaTrilha(){
-          trilha.play();
-          trilha.volume = 0.2;
-     }
-     function pauseTrilha(){
-          trilha.pause();
-     }
-     function executaExplosao(){
-          audioExplosao.play();
-     }
-     executaTrilha();
-     
-     var jogo = {};
-     var velocidade = 3;
+     //GameLoop Methods Config
+     var game = {};
+     var velocidade = 5;
      var podeAtirar = true;
-     var TECLA = {
-         W:87,
-         S:83,
-         SPACE:32
-     };
-    
-    jogo.pressionou = [];
 
-    $(document).keydown(function(e){
-         jogo.pressionou[e.which] = true;
-    });
-    $(document).keyup(function(e){
-         jogo.pressionou[e.which] = false;
-    });
+     game.timer = setInterval(gameLoop, Math.floor(Math.random() * 30) + 15);
 
-    jogo.timer = setInterval(loop, 10);
+     function gameLoop(){
+          scrollBackground();
+          targetPlayerAndShoot();
+          electricBarrier();
+          generateCoins();
+          //    colisao();
+          gameover(); 
+          vitoria();
+     }
+     //End GameLoop Methods Config
 
-   function moveFundo(){
-       esquerda = parseInt($(".fundoGame").css("background-position"));
-       $(".fundoGame").css("background-position", esquerda -1);
-   }
-
-   function moveJogador(){
-        //Teclas pressionadas
-        if(jogo.pressionou[TECLA.W]){
-             var topo = parseInt($(".player").css("top"));
-             $(".player").css("top", topo -10);
-             if(topo<=-150){
-                  $(".player").css("top", topo +10);
-             }
-        }
-        if(jogo.pressionou[TECLA.S]){
-             var topo = parseInt($(".player").css("top"));
-             $(".player").css("top", topo +10);
-             if(topo>=270){
-                  $(".player").css("top", topo -10);
-             }
-        }
-        if(jogo.pressionou[TECLA.SPACE]){
-             disparo();
-        }
-   }
-
-   function disparo(){
-        if(podeAtirar==true){
-          podeAtirar=false;
-          topo = parseInt($(".player").css("top"));
-          posicaoX = parseInt($(".player").css("left"));
-          tiroX = posicaoX + 120;
-          topoTiro = topo - 230;
-          $(".fundoGame").append("<div class='disparo'></div>");
-          $(".disparo").css("top", topoTiro);
-          $(".disparo").css("left", tiroX);
-          var tempoDisparo = window.setInterval(executaDisparo, 30);
-        }
-        function executaDisparo(){
-          posicaoX = parseInt($(".disparo").css("left"));
-          $(".disparo").css("left", posicaoX+15);
-          if(posicaoX>910){
-               window.clearInterval(tempoDisparo);
-               tempoDisparo = null;
-               $(".disparo").remove();
-               podeAtirar = true;
+     //Movement Area (Using Lerp to Move through Y Axis)
+     var mouseX=$("#gameplay").innerWidth()/2;
+     var mouseY=$("#gameplay").innerHeight()/2;
+     
+     var player = {
+          el:$('#player'),
+          w:100, h:115,
+          x:$("#gameplay").innerWidth() * .10, 
+          y:$("#gameplay").innerHeight() / 2,
+          
+          update:function(){
+               translateX = this.x-this.w/2;
+               translateY = this.y-this.h/2 >= 300 ? 300 : this.y-this.h/2;
+               this.el.css({
+                    'transform':'translate('+translateX+'px, '+translateY+'px)'
+               });
           }
-             
-        }
-   }
+     }
 
-   function moveMissil(){
-        posicaoX = parseInt($(".missile").css("left"));
-        $(".missile").css("left", posicaoX - velocidade);
-        if(posicaoX <= 0){
-            var posicaoY = parseInt(Math.random()*300);
-            $(".missile").css("left",800);
-            $(".missile").css("top",posicaoY);
-        }
-   }
+     $(window).mousemove(function(e){
+          mouseX = e.clientX / 2;
+          mouseY = e.clientY / 2;
+     })
+    
+     function move(){
+               // player.x = lerp(player.x, mouseX, 0.1);
+               player.y = lerp(player.y, mouseY, 0.1);
+               player.update() 
+     }
+     
+     function lerp (start, end, amt){
+          return (1-amt)*start+amt*end
+     }
 
-   function moveObstaculo(){
-        posicaoX = parseInt($(".obstacle").css("left"));
-        $(".obstacle").css("left", posicaoX - (velocidade/2));
-        if(posicaoX <= 0){
-            $(".obstacle").css("left",800);
-        }
-   }
+     setInterval (move,1000/60)
+     //End Movement Area
+  
+
+     //In Game Objects Control
+     function scrollBackground(){
+          esquerda = parseInt($(".backgroundGame").css("background-position"));
+          $(".backgroundGame").css("background-position", esquerda -1);
+     }
+
+
+     function targetPlayerAndShoot(){
+          posX = parseInt($(".missile").css("left"));
+          $(".missile").css("left", posX - 5);
+          $(".missile").css("background-position", esquerda -1);
+          
+          if(posX <= -300){
+               $(".missile").css("left",1100);
+               $(".missile").css("top",  ($('.player').offset().top + 15) / 2);
+          }
+     }
+
+     function electricBarrier(){
+          posX = parseInt($(".obstacle").css("left"));
+          $(".obstacle").css("left", posX - 5);
+          if(posX <= -250){
+               var posY = parseInt(Math.random() * 30);
+               $(".obstacle").css("left",1050);
+          }
+     }
+
+     function generateCoins()
+     {
+          posX = parseInt($(".coin").css("left"));
+          $(".coin").css("left", posX - 5);
+          
+          if(posX <= -50){
+               var posY = parseInt(Math.random() * 300);
+               $(".coin").css("left",1100);
+               $(".coin").css("top",50);
+
+          }
+     }
+
 
 //    function colisao(){
 //         var colisao1 = ($(".player")).collision($(".inimigo"));
@@ -131,8 +127,8 @@ function start(){
 
 //              executaExplosao();
 
-//              pontos-=5;
-//              document.getElementById("pontos").innerHTML = String(pontos);
+//              score-=5;
+//              document.getElementById("score").innerHTML = String(score);
 
 //              posicaoY = parseInt(Math.random()*300);
 //              $(".inimigo").css("left", 700);
@@ -151,8 +147,8 @@ function start(){
 
 //              executaExplosao();
 
-//              pontos-=5;
-//              document.getElementById("pontos").innerHTML = String(pontos);
+//              score-=5;
+//              document.getElementById("score").innerHTML = String(score);
 
 //              $(".npc").css("left", 800);
 //              $(".npc").css("top", 130);
@@ -163,8 +159,8 @@ function start(){
 //         if(colisao3.length>0){
 //              $(".amigo").hide();
 //              var tempoRecriar = window.setInterval(recriar, 4000);
-//              pontos+=5;
-//              document.getElementById("pontos").innerHTML = String(pontos);
+//              score+=5;
+//              document.getElementById("score").innerHTML = String(score);
 //         }
 //         function recriar(){
 //             $(".amigo").show();
@@ -197,14 +193,14 @@ function start(){
 //              amigoY = parseInt($(".amigo").css("top"));
 //              explosaoAmigo(amigoX, amigoY);
 //              $(".amigo").hide();
-//              pontos-=1;
-//              document.getElementById("pontos").innerHTML = String(pontos);
+//              score-=1;
+//              document.getElementById("score").innerHTML = String(score);
 //              var tempoRecriar = window.setInterval(recriar, 6000);
 //         }
 
 //    }
    function explosao(inimigoX, inimigoY){
-        $(".fundoGame").append("<div class='explosao'></div>");
+        $(".backgroundGame").append("<div class='explosao'></div>");
         $(".explosao").css("background-image", "url(image/explosao.png)");
         var div = $(".explosao");
         div.css("top",inimigoY+150);
@@ -219,7 +215,7 @@ function start(){
         }
    }
    function explosaoPlayer(playerX, playerY){
-        $(".fundoGame").append("<div class='explosaoPlayer'></div>");
+        $(".backgroundGame").append("<div class='explosaoPlayer'></div>");
         $(".explosaoPlayer").css("background-image","url(image/explosao-player.png)");
         var div2 = $(".explosaoPlayer");
         div2.css("top",playerY+300);
@@ -234,7 +230,7 @@ function start(){
         }
    }
    function explosaoNpc(npcX, npcY){
-        $(".fundoGame").append("<div class='explosaoNpc'></div>");
+        $(".backgroundGame").append("<div class='explosaoNpc'></div>");
         $(".explosaoNpc").css("background-image", "url(image/explosao.png");
         var div3 = $(".explosaoNpc");
         div3.css("top", npcY+400);
@@ -249,7 +245,7 @@ function start(){
         }
    }
    function explosaoAmigo(amigoX, amigoY){
-        $(".fundoGame").append("<div class='explosaoAmigo'></div>");
+        $(".backgroundGame").append("<div class='explosaoAmigo'></div>");
         $(".explosaoAmigo").css("background-image", "url(image/sangue.png");
         var div4 = $(".explosaoAmigo");
         div4.css("top", amigoY+500);
@@ -263,38 +259,55 @@ function start(){
         }
    }
    function gameover(){
-        if(pontos < 0){
-          window.clearInterval(jogo.timer);
-          jogo.timer = null;
-          pontos = 0;
+        if(score < 0){
+          window.clearInterval(game.timer);
+          game.timer = null;
+          score = 0;
           $(".player").remove();
           $(".npc").remove();
           $(".inimigo").remove();
           $(".amigo").remove();
-          $(".pontos").remove()
+          $(".score").remove()
           $("#inicio").show();
           pauseTrilha();
         }
    }
-   function vitoria(){
-        if(pontos>=10){
-             window.clearInterval(jogo.timer);
-             jogo.timer = null;
+    function vitoria(){
+        if(score>=10){
+             window.clearInterval(game.timer);
+             game.timer = null;
              $(".obstacle").remove();
              $(".missile").remove();
              $(".scientist").remove();
-             $(".fundoGame").append("<div class='vitoria'><h1>Você ganhou!!</h1></div>");
+             $(".backgroundGame").append("<div class='vitoria'><h1>Você ganhou!!</h1></div>");
              pauseTrilha();
           }
-   }
+    }
 
-   function loop(){
-        moveFundo();
-        moveJogador();
-        moveMissil();
-        moveObstaculo();
-        colisao();
-        gameover();
-        vitoria();
-   }
+    //Audio Area
+    var trilha = document.getElementById("background-music");
+    var audioExplosao = document.getElementById("audioExplosao");
+
+    //Music Area
+    function executaTrilha(){
+        trilha.play();
+        trilha.volume = 0.2;
+    }
+    function pauseTrilha(){
+        trilha.pause();
+    }
+    executaTrilha();
+
+    //SFX Area
+    function executaExplosao(){
+        audioExplosao.play();
+    }
+       
+    //Utils
+     function getCurrentPlayerY(el) {
+          var rect = $('.player').getBoundingClientRect(),
+          scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+          scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+          return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+     }
 }
